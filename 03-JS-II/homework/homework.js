18d63b93563f2a0c66baf40bfe16c47f5411f2b5
CristhianDayde <cristhiandayde@gmail.com>
2022-08-08 00:22:04 -0300

    tareajs2-08-08-2022--00-21

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -110,12 +110,11 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(numero<50){
-    return "true";
-  }if(numero>20){
-    return "true";
-  }else
-      return "false"
+  if(numero<50&& numero> 20){
+    return true;
+  }
+  else
+      return false;
 }
 
 function esEntero(numero) {
@@ -126,14 +125,29 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero - Math.floor(numero) === 0) {
+    return true
+} else {
+    return false
 }
-
+  
+}
 function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero % 3===0 && numero % 5===0){
+    return "fizzbuzz";
+  }if(numero % 3===0){
+  return "fizz";
+  }if(numero% 5===0){
+    return "buzz";
+  }else{
+    return numero
+  }
 }
+
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
@@ -142,6 +156,22 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  
+    if (Math.sign(num1, num2, num3)===1 && num1>num2,num3){
+      return "Número 1 es mayor y positivo"
+    }if(Math.sign.num1,num2,num3===-1){
+      return "Hay negativos"
+    }if(num3>num2 && num3>num1){
+      return num3 +1
+    }if(Math.sign(num1, num2, num3) === 0 || -0){
+      return "Error";
+    }else{
+      return false;
+    }
+      
+
+  
+  
 }
 
 function esPrimo(numero) {
